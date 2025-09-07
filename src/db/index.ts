96c7f41e5dc6e352c0c4431f6ecdfa2517a604f6
Aadi <83467783+sinarck@users.noreply.github.com>
2025-09-07 02:38:45 -0500

    feat: basic database schema

---
+++ src/db/index.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/libsql";
+
+if (!process.env.TURSO_DATABASE_URL || !process.env.TURSO_AUTH_TOKEN) {
+  throw new Error("TURSO_DATABASE_URL and TURSO_AUTH_TOKEN must be set");
+}
+
+const db = drizzle({
+  connection: {
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
+  },
+});
+
+export default db;
