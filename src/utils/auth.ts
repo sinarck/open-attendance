6e0d209da757df71f92651de2a9d32e4f043a4c5
Aadi <83467783+sinarck@users.noreply.github.com>
2025-09-19 00:04:05 -0500

    feat: disable signups

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,15 +1,14 @@
-import { betterAuth } from "better-auth";
+import { APIError, betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { username } from "better-auth/plugins";
+import { createAuthMiddleware, username } from "better-auth/plugins";
 import db from "@/db";
 import * as schema from "@/db/schema/auth-schema";
 
 export const auth = betterAuth({
-  // Enable classic email + password endpoints
   emailAndPassword: {
     enabled: true,
+    disableSignUp: true,
   },
-  // Add username support (adds hooks, /sign-in/username, etc.)
   plugins: [username()],
   database: drizzleAdapter(db, {
     provider: "sqlite",
