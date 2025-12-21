dc9aa382dfdc1a191f9fc1348bdf34b581aec9e6
Aadi <83467783+sinarck@users.noreply.github.com>
2025-12-20 23:22:50 -0600

    feat: new and improved `theme-toggle.tsx` component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,11 +25,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import ThemeToggle from "@/components/ui/theme-toggle";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-background">
       Placeholder page
+      <ThemeToggle />
       <Button>Check in</Button>
     </div>
   );
