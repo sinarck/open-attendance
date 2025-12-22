fb788174df12e5e771ea810f2cf838527b9aedf1
Aadi <83467783+sinarck@users.noreply.github.com>
2025-12-22 00:22:49 -0600

    feat: add `drizzle orm` support

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,8 +1,11 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { vercel } from "@t3-oss/env-nextjs/presets-zod";
+import z from "zod";
 
 export const env = createEnv({
-  server: {},
+  server: {
+    DATABASE_URL: z.url().nonempty(),
+  },
   client: {},
   extends: [vercel()],
   experimental__runtimeEnv: process.env,
