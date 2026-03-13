42b005c98ce48568e3709e3a9693e4e8f27744b9
Aadi <aadisang@outlook.com>
2026-03-13 01:07:51 -0500

    chore: remove Neon/Drizzle code and dependencies

--- src/db/schema/base.ts
+++
@@ -1,7 +0,0 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
-
-export const membersTable = pgTable("members", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  identifier: varchar({ length: 255 }).notNull(),
-});
